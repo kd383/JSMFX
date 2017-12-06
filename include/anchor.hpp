36ed9869956d0b753bade6d67848f9f94ac36615
kd383 <dongkun_29@hotmail.com>
2017-12-05 23:56:19 -0500

    recover

--- include/anchor.hpp
+++ include/anchor.hpp
@@ -9,6 +9,7 @@
 using std::string;
 using arma::mat;
 using arma::colvec;
+using arma::rowvec;
 using arma::uvec;
 using arma::ivec;
 using arma::conv_to;
@@ -17,97 +18,157 @@ using std::vector;
 const float GAMMA = 3.0;
 
 class anchor{
-	private:
-		int n_, k_;
-		mat X_;
-		mat R_;
-		colvec colSum_;
-		mat RXbar_;
-		uvec anchor_;
-		mat B_;
-		mat A_;
+    private:
+    int n_, k_;
+    mat X_;
+    mat R_;
+    colvec colSum_;
+    mat RXbar_;
+    uvec anchor_;
+    mat B_;
+    mat A_;
+    mat C_;
+    mat Cbar_;
+    bool isLowRank_;
 
 
-	public:
-		anchor(const mat X) { X_ = mat(X); n_ = X_.n_rows; k_ = X_.n_cols;}
-		anchor(const char* str) { X_.load(str); n_ = X_.n_rows; k_ = X_.n_cols;}
-		const mat& X() const { return X_; }
-		const colvec& ColSum() const { return colSum_; }
-		void CalculateColSum();
-		const mat& R() const { return R_; }
-		void CalculateR();
-		const uvec Anchor() const { return anchor_; }
-		void FindAnchor();
-		const mat& B() const { return B_; }
-		void FindB();
-		const mat& A() const { return A_; }
-		void FindA();
+    public:
+    anchor(const mat X);
+    anchor(const mat C, const int k):anchor(C) { k_ = k; }
+    anchor(const char* str);
+    anchor(const char* str, const int k):anchor(str) { k_ = k;}
+    const mat& C() const { return C_; }
+    const mat& X() const { return X_; }
+    const colvec& ColSum() const { return colSum_; }
+    const mat& R() const { return R_; }
+    const uvec Anchor() const { return anchor_; }
+    const mat& B() const { return B_; }
+    const mat& A() const { return A_; }
+    void CalculateColSum();
+    void CalculateR();
+    void FindAnchor();
+    void FindB();
+    void FindA();
 };
+
+anchor::anchor(const mat X)
+{
+    if ( X.n_rows == X.n_cols) {
+        C_ = mat(X);
+        n_ = X.n_rows;
+        k_ = 20;
+        isLowRank_ = false;
+    } else {
+        X_ = mat(X);
+        n_ = X.n_rows;
+        k_ = X.n_cols;
+        isLowRank_ = true;
+    }
+}
+
+anchor::anchor(const char* str)
+{
+    mat X;
+    X.load(str);
+    if ( X.n_rows == X.n_cols) {
+        C_ = mat(X);
+        n_ = X.n_rows;
+        k_ = 20;
+        isLowRank_ = false;
+    } else {
+        X_ = mat(X);
+        n_ = X.n_rows;
+        k_ = X.n_cols;
+        isLowRank_ = true;
+    }
+}
 
 void anchor::CalculateColSum()
 {
-	colvec xsum = conv_to<colvec>::from(sum(X_,0));
-	colSum_ = X_*xsum;
-	return;
+    if (isLowRank_){ 
+        colvec xsum = conv_to<colvec>::from(sum(X_,0));
+        colSum_ = X_*xsum;
+        return;
+    } else {
+        colSum_ = sum(C_,1);
+        return;
+    }
 }
 
 void anchor::CalculateR()
 {
-	R_ = mat(X_);
-	QR(R_);
-	R_ = trimatu(R_.rows(0,k_-1));
-	return;
+    R_ = mat(X_);
+    QR(R_);
+    R_ = trimatu(R_.rows(0,k_-1));
+    return;
 }
 
 void anchor::FindAnchor()
 {
-	RXbar_ = X_.each_col()/colSum_;
-	RXbar_ = R_*RXbar_.t();
-	vector<int> piv(n_); 
-	QRCP(RXbar_, piv);
-	piv.resize(k_);
-	anchor_ = conv_to<uvec>::from(piv) - 1;
+    vector<int> piv(n_);
+    if (isLowRank_) {
+        RXbar_ = X_.each_col()/colSum_;
+        RXbar_ = R_*RXbar_.t();
+        QRCP(RXbar_, piv);
+    } else {
+        Cbar_ = C_.each_row()/conv_to<rowvec>::from(colSum_);
+        QRCP(Cbar_, piv);
+    }
+    piv.resize(k_);
+    anchor_ = conv_to<uvec>::from(piv) - 1;
 
-	return;
+    return;
 }
 
 void anchor::FindB()
 {
-	mat H = RXbar_.cols(anchor_);
-	mat HtH = H.t()*RXbar_;
-	mat HtHS = HtH.cols(anchor_);
-	mat y0 = arma::solve(HtHS,HtH);
-	mat F = arma::inv(GAMMA*HtHS + arma::eye(arma::size(HtHS)));
-	vec f(k_);
+    mat F, y0, HtH, H;
+    if (isLowRank_) {
+        H = RXbar_.cols(anchor_);
+        HtH = H.t()*RXbar_;
+    } else {
+        mat H = Cbar_.cols(anchor_);
+        HtH = H.t()*Cbar_;
+    }
+    mat HtHS = HtH.cols(anchor_);
+    y0 = arma::solve(HtHS,HtH);
+    F = arma::inv(GAMMA*HtHS + arma::eye(arma::size(HtHS)));
+    vec f(k_);
 
-	ivec ind(n_);
-	ind.fill(-1);
-	for (int i = 0; i < anchor_.size(); i++)
-		ind[anchor_[i]] = i;
+    ivec ind(n_);
+    ind.fill(-1);
+    for (int i = 0; i < anchor_.size(); i++)
+        ind[anchor_[i]] = i;
 
-	mat B(k_, n_, arma::fill::zeros);
-	for (int i = 0; i < n_; i++) {
-		if (ind[i]>=0)
-			B(ind[i],i) = 1;
-		else {
-			f = GAMMA*HtH.col(i);
-			B.col(i) = ADMM_DR(F, f, y0.col(i));
-		}
-	}
-	arma::rowvec denom = (B*colSum_).t();
-	B_ = B.t();
-	B_.each_col() %= colSum_;
-	B_.each_row() /= denom;
+    mat B(k_, n_, arma::fill::zeros);
+    for (int i = 0; i < n_; i++) {
+        if (ind[i]>=0)
+            B(ind[i],i) = 1;
+        else {
+            f = GAMMA*HtH.col(i);
+            B.col(i) = ADMM_DR(F, f, y0.col(i));
+        }
+    }
+    arma::rowvec denom = (B*colSum_).t();
+    B_ = B.t();
+    B_.each_col() %= colSum_;
+    B_.each_row() /= denom;
 
-	return;
+    return;
 }
 
 void anchor::FindA()
 {
-	mat XS = X_.rows(anchor_);
-	A_ = XS*XS.t();
-	vec denom = arma::diagvec(B_.rows(anchor_));
-	A_.each_col() /= denom;
-	A_.each_row() /= denom.t();
+    if (isLowRank_) {
+        mat XS = X_.rows(anchor_);
+        A_ = XS*XS.t();
+    } else {
+        A_ = C_.submat(anchor_,anchor_);
+    }
+    vec denom = arma::diagvec(B_.rows(anchor_));
+    A_.each_col() /= denom;
+    A_.each_row() /= denom.t();
+
+   return;
 }
 #endif
